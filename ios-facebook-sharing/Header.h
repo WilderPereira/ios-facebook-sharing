310628fe7bc96570e47e556c885941593138f9c7
Wilder Pereira <wilder_roberto@hotmail.com>
2016-07-18 21:36:56 -0300

    Setup facebook sdk

---
+++ ios-facebook-sharing/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  ios-facebook-sharing
+//
+//  Created by Wilder Pereira on 18/07/16.
+//  Copyright © 2016 Wilder Pereira. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+
+#endif /* Header_h */
